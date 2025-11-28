ed3d68a2f627c11df9015abb7321a136af1906b9
odrgues <odrgues@gmail.com>
2025-11-28 18:11:36 -0300

    retirada de imports nao utilizados

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import GlobalStyles from "./components/GlobalStyles";
-import Header from "./components/Header";
 import Footer from "./components/Footer";
 
 import HomePage from "./pages/HomePage";
